dba92b68f437f0f1850183100ea9fb07d1e4143d
Tushar Borole <tushariscoolster@gmail.com>
2015-05-27 01:46:08 +0530

    theme removed

--- conf/config.js
+++
@@ -1,7 +0,0 @@
-
-
- angular.module('Config', [])
-
-.constant('$enviornment', {analyticsAppId:'UA-XXXXXXXX-1',facebookAppId:'803251069707321',jsonurl:'',name:'development',backendurl:'http://MYSITE.nisostech.com/MYSITE-backend-account/',backendurlngo:'http://karma-staging.nisostech.com/karma-be-ngo/',backendurlsocial:'http://karma-staging.nisostech.com/social/',urlname:'development',viewbasepath:'',jsonpath:'/json/detailJson/'})
-
-;
