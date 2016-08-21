5d9c180f4ea3b4a4664860bf95fe3ad5df87ff9a
Tushar <tushariscoolster@gmail.com>
2016-08-21 16:16:28 -0500

    version changes intial

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    
-    
-         $("#typed").typed({
-            strings: ["Javascript", "HTML", "CSS", "PHP", "AngularJS","JQUERY", "Phonegap"],
-            typeSpeed: 0,
-              loop: true
-        });
-})
